f5800cc8b55d99a5c4a0eedcfd9f952b92c9b871
umutdundar99 <umutdundar37@gmail.com>
2025-03-24 21:40:03 +0300

    gpt2 encoding for nano

--- sentiment_analysis/data/loader.py
+++ sentiment_analysis/data/loader.py
@@ -4,20 +4,18 @@ import lightning as L
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset
+import tiktoken
 
 
 class SentimentAnalysisDataset(Dataset):
-    def __init__(self, csv_path: str, max_length: int = 512):
+    def __init__(self, csv_path: str, max_length: int = 256):
         self.data = pd.read_csv(csv_path)
-        texts = self.data["text"].tolist()
-        self.labels = self.data["label"].tolist()
-
-        all_text = "".join(texts)
-        self.chars = sorted(list(set(all_text)))
-        self.stoi = {ch: i + 1 for i, ch in enumerate(self.chars)}
-        self.itos = {i + 1: ch for i, ch in enumerate(self.chars)}
         self.max_length = max_length
-        self.vocab_size = len(self.stoi) + 1
+        
+        self.labels = self.data["customer_sentiment"].tolist()
+        self.enc = tiktoken.get_encoding("gpt2")
+        self.label_map = {'negative': 0, 'neutral': 1, 'positive': 2}
+        self.data['label'] = self.data['customer_sentiment'].map(self.label_map)
 
     def encode(self, text):
         encoded = [self.stoi.get(ch, 0) for ch in text]
@@ -30,13 +28,15 @@ class SentimentAnalysisDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        text = self.data.iloc[idx]["text"]
-        label = self.labels[idx]
-        encoded_text = self.encode(text)
-
-        input_tensor = torch.tensor(encoded_text, dtype=torch.long)
+        conversation = self.data.iloc[idx]['conversation']
+        label = self.data.iloc[idx]['label']
+        token_ids = self.enc.encode_ordinary(conversation)
+        if len(token_ids) > self.max_length:
+            token_ids = token_ids[:self.max_length]
+        else:
+            token_ids += [0] * (self.max_length - len(token_ids))
+        input_tensor = torch.tensor(token_ids, dtype=torch.long)
         label_tensor = torch.tensor(label, dtype=torch.long)
-
         return input_tensor, label_tensor
 
 
