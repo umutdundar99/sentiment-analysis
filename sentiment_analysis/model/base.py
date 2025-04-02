4fd335c47ceffbee3e310b031526dc91d96e004d
umutdundar99 <umutdundar37@gmail.com>
2025-04-02 16:09:07 +0300

    Update configuration for GPT-2 and NanoGPT; add Focal Loss implementation and adjust training parameters

--- sentiment_analysis/model/base.py
+++ sentiment_analysis/model/base.py
@@ -1,9 +1,11 @@
-from typing import Any, Dict
+from typing import Any, Optional
 
 import lightning as L
 import torch
 import torchmetrics
+import wandb
 from torchmetrics.classification import (
+    ConfusionMatrix,
     MulticlassAccuracy,
     MulticlassF1Score,
     MulticlassPrecision,
@@ -12,88 +14,95 @@ from torchmetrics.classification import (
 
 
 class BaseModule(L.LightningModule):
-    def __init__(
-        self,
-    ):
+    def __init__(self, num_classes: Optional[int] = None):
         super().__init__()
-        self.train_metrics = torchmetrics.MetricCollection(
-            {
-                "Accuracy": MulticlassAccuracy(num_classes=3),
-                "Precision": MulticlassPrecision(num_classes=3),
-                "Recall": MulticlassRecall(num_classes=3),
-                "F1": MulticlassF1Score(num_classes=3),
-            }
+
+        self.num_classes = num_classes
+
+        metrics = {
+            "accuracy": MulticlassAccuracy(num_classes=self.num_classes),
+            "precision": MulticlassPrecision(num_classes=self.num_classes),
+            "recall": MulticlassRecall(num_classes=self.num_classes),
+            "f1": MulticlassF1Score(num_classes=self.num_classes),
+        }
+
+        self.train_metrics = torchmetrics.MetricCollection(metrics, prefix="train/")
+        self.val_metrics = torchmetrics.MetricCollection(metrics, prefix="val/")
+
+        self.train_confmat = ConfusionMatrix(
+            task="multiclass", num_classes=self.num_classes
         )
-        self.val_metrics = torchmetrics.MetricCollection(
-            {
-                "Accuracy": MulticlassAccuracy(num_classes=3),
-                "Precision": MulticlassPrecision(num_classes=3),
-                "Recall": MulticlassRecall(num_classes=3),
-                "F1": MulticlassF1Score(num_classes=3),
-            }
+        self.val_confmat = ConfusionMatrix(
+            task="multiclass", num_classes=self.num_classes
         )
 
-    def calculate_metric(
-        self, logits: torch.Tensor, y: torch.Tensor, metric: Dict[str, Any]
-    ):
-        """
-        Calculates the accuracy of the model.
+        self.class_labels = ["negative", "neutral", "positive"]
 
-        Args:
-            logits (torch.Tensor): The model predictions.
-            y (torch.Tensor): The true labels.
-
-        Returns:
-            torch.Tensor: The accuracy of the model.
-        """
-
-        for name, _metric in metric.items():
-            _metric = _metric.to(logits.device)
-            _metric.update(logits, y)
-            self.log(name, _metric, on_epoch=True, on_step=False)
-
-    def reset_metrics(self, metric: Dict[str, Any]):
-        for _metric in metric.values():
-            _metric.reset()
-
-    def forward(self, x):
-        logits, _ = self.model(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        logits = self.model(x)
         return logits
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
         x, y = batch
-        logits = self.model(x, y)
+        logits = self.model(x)
         loss = self.criterion(logits, y)
 
-        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
-        self.calculate_metric(logits, y, self.train_metrics)
-        self.log(
-            "train/accuracy",
-            self.train_metrics["Accuracy"],
-            on_step=False,
-            on_epoch=True,
-            prog_bar=True,
-        )
+        preds = torch.argmax(logits, dim=1)
+        train_results = self.train_metrics(preds, y)
+        confmat = self.train_confmat(preds, y)
+
+        self.log_dict(train_results, prog_bar=True)
+
+        per_class_acc = confmat.diag() / confmat.sum(dim=1)
+
+        for i, acc in enumerate(per_class_acc):
+            self.log(f"train/acc_{self.class_labels[i]}", acc, prog_bar=True)
+
+        self.log("loss/train", loss, on_step=True, on_epoch=True, prog_bar=True)
 
         return loss
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
         x, y = batch
-        logits = self.model(x, y)
+        logits = self.model(x)
         loss = self.criterion(logits, y)
-        self.log("val_loss", loss, on_epoch=True, prog_bar=True)
-        self.log(
-            "val/accuracy",
-            self.val_metrics["Accuracy"],
-            on_step=False,
-            on_epoch=True,
-            prog_bar=True,
-        )
-        self.calculate_metric(logits, y, self.val_metrics)
+
+        preds = torch.argmax(logits, dim=1)
+        self.val_metrics.update(preds, y)
+        print(f"preds: {preds}")
+        print(f"groun: {y}")
+        print(f"Epoch: {self.current_epoch} ---------------------------------------")
+        self.val_confmat.update(preds, y)
+
+        self.log("loss/val", loss, on_step=False, on_epoch=True, prog_bar=True)
+
         return loss
 
     def on_train_epoch_end(self):
-        self.reset_metrics(self.train_metrics)
+        self.train_metrics.reset()
 
     def on_validation_epoch_end(self):
-        self.reset_metrics(self.val_metrics)
+        val_results = self.val_metrics.compute()
+        self.log_dict(val_results, prog_bar=True)
+
+        confmat = self.val_confmat.compute()
+        per_class_acc = confmat.diag() / confmat.sum(dim=1)
+
+        for i, acc in enumerate(per_class_acc):
+            self.log(f"val/acc_{self.class_labels[i]}", acc, prog_bar=True)
+
+        confmat_table = wandb.Table(
+            columns=["Predicted"] + self.class_labels,
+            data=[
+                [self.class_labels[i]] + confmat[i].tolist()
+                for i in range(self.num_classes)
+            ],
+        )
+        self.logger.experiment.log({"val/confusion_matrix": confmat_table})
+
+        self.val_metrics.reset()
+        self.val_confmat.reset()
+
+    # def configure_optimizers(self) -> torch.optim.Optimizer:
+    #     optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4)
+    #     return optimizer
